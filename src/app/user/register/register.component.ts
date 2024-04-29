9013276364cbd9bd64a2e348764af0c882c2052b
nnaamm1 <68DCHT20095@utteduvn.onmicrosoft.com>
2024-04-29 18:06:46 +0700

    Authentication and custom validator for form

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import IUser from 'src/app/models/user.model';
+import { AuthService } from 'src/app/services/auth.service';
+import { RegisterValidators } from '../validators/register-validators';
+import { EmailTaken } from '../validators/email-taken';
 
 @Component({
   selector: 'app-register',
@@ -7,33 +13,59 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-  registerForm = new FormGroup({
-    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
-    email: new FormControl('', [Validators.required, Validators.email]),
-    age: new FormControl('', [
-      Validators.required,
-      Validators.min(18),
-      Validators.max(120),
-    ]),
-    password: new FormControl('', [
-      Validators.required,
-      Validators.pattern(/((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{8,})/g),
-    ]),
-    confirm_password: new FormControl('', [Validators.required]),
-    phoneNumber: new FormControl('', [
-      Validators.required,
-      Validators.minLength(13),
-      Validators.maxLength(14),
-    ]),
-  });
+  constructor(private auth: AuthService, private emailTaken: EmailTaken) {}
+
+  inSubmission = false;
+
+  registerForm = new FormGroup(
+    {
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      email: new FormControl(
+        '',
+        [Validators.required, Validators.email],
+        [this.emailTaken.validate]
+      ),
+      age: new FormControl<number | null>(null, [
+        Validators.required,
+        Validators.min(18),
+        Validators.max(120),
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{8,})/g),
+      ]),
+      confirm_password: new FormControl('', [Validators.required]),
+      phoneNumber: new FormControl('', [
+        Validators.required,
+        Validators.minLength(13),
+        Validators.maxLength(14),
+      ]),
+    },
+    [RegisterValidators.match('password', 'confirm_password')]
+  );
 
   showAlert = false;
   alertMessage = 'Please wait...';
   alertColor = 'blue';
 
-  register() {
+  async register() {
     this.showAlert = true;
     this.alertMessage = 'Please wait...';
     this.alertColor = 'blue';
+    this.inSubmission = true;
+
+    try {
+      await this.auth.createUser(this.registerForm.value as IUser);
+    } catch (error) {
+      console.error(error);
+
+      this.alertMessage = 'An error occurred. Please try again.';
+      this.alertColor = 'red';
+      this.inSubmission = false;
+      return;
+    }
+
+    this.alertMessage = 'Registration successful!';
+    this.alertColor = 'green';
   }
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCw_41TJGbjztBYzX6ebfDVuKKdTEFdzvk',
+    authDomain: 'clips-998a4.firebaseapp.com',
+    projectId: 'clips-998a4',
+    storageBucket: 'clips-998a4.appspot.com',
+    messagingSenderId: '564490440991',
+    appId: '1:564490440991:web:1a6fbe7ab59b8cc241e02a',
+  },
+};
